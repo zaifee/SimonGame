ea13c6895426918000f3834be51e1e0539e509e9
Mohd Nawaz <94606718+zaifee@users.noreply.github.com>
2022-08-02 01:30:28 -0700

    changes in sounds.

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ playSound(userChoosenColours);
 }
 
 function playSound(name){
-    var audio = new Audio("sounds/" + name + ".mp3")
+    var audio = new Audio(name + ".mp3")
     audio.play();
    
 }
